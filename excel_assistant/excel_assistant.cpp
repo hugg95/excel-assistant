8cbca241fd26d22aa03bd0b6b3268f7037c14fe7
victor <lncwwn@gmail.com>
2015-01-21 00:54:45 +0800

    added new functions and fixed the split bugs

--- excel_assistant/excel_assistant.cpp
+++ excel_assistant/excel_assistant.cpp
@@ -21,7 +21,9 @@ void list_files(vector<CStringW> & files_arr, LPCTSTR pstr);
 // split a string using the specified symbol
 vector<CStringW> split(CStringW str, char symbol);
 
-void _parse_line(LPWSTR line, short index);
+BOOL belongs_to(CStringW str, vector<CStringW> target_array);
+
+void _parse_line(CStringW line, short index);
 
 // create a new file and write into the content
 void write_file();
@@ -32,9 +34,9 @@ void read_file(LPCWSTR file) {
     err_no = _wfopen_s(&fp, file, _T("r, ccs=UNICODE"));
     if (0 == err_no) {
         //wchar_t c;
-        wchar_t line[100];
-        while ((fgetws(line, 100, fp)) != NULL) {
-            _parse_line(line, 2);
+        wchar_t line[1000];
+        while ((fgetws(line, 1000, fp)) != NULL) {
+            _parse_line(line, 14);
         }
         //c = fgetwc(fp);
         /*while (c != WEOF) {
@@ -71,14 +73,36 @@ void list_files(vector<CStringW> & files_arr, LPCTSTR pstr) {
     finder.Close();
 }
 
-void _parse_line(LPWSTR line, short index) {
-    vector<CStringW> v = split(line, ',');
-    for (unsigned int i = 0; i < v.size(); i++) {
-        printf("%S\n", v.at(i));
+void _parse_line(CStringW line, short index) {
+    CStringW copy_line = line;
+    int fquote_index = line.Find('\"');
+    if (fquote_index != -1) {
+        int lquote_index = 0, _index;
+        while ((_index = line.Find('\"')) != -1) {
+            line = CStringW(line.Right(line.GetLength() - _index - 1));
+            lquote_index += _index + 1;
+        }
+        int len = lquote_index - fquote_index;
+        CStringW left = copy_line.Left(fquote_index);
+        line = CStringW(copy_line.Right(copy_line.GetLength() - fquote_index));
+        CStringW right = CStringW(line.Right(line.GetLength() - len));
+        line = CStringW(line.Left(len));
+        line.Replace('\,', '\t');
+        line = left + line + right;
     }
+    
+    vector<CStringW> arr = split(line, ',');
+    for (int i = 0; i < arr.size(); i++) {
+        if (arr.at(i).Find('\t') != -1) {
+            arr.at(i).Replace('\t', ',');
+        }
+    }
+    CStringW specifed_str = arr.at(index);
+    printf("%S\n", specifed_str);
+
 }
 
-BOOL belongs_to(LPWSTR str, vector<LPWSTR> target_array) {
+BOOL belongs_to(CStringW str, vector<CStringW> target_array) {
     int i = 0, len = target_array.size();
     for (; i < len; i++) {
         if (target_array.at(i) == str) {
@@ -100,8 +124,22 @@ vector<CStringW> split(CStringW str, char symbol) {
     return result;
 }
 
-// create a new file
-void write_file(char* file) {
+void write_file(CStringW path, CStringW line) {
+    FILE* fp;
+    errno_t err_no = _wfopen_s(&fp, path, _T("r"));
+    // no such file or directory
+    if (err_no == 2) {
+        if (!CreateDirectoryW(path, NULL)) {
+            printf("create directory filed (%d)\n", GetLastError());
+            return;
+        }
+    }
+
+    //fwrite(&fp, sizeof(line), );
+}
+
+void init_env() {
+
 }
 
 int _tmain(int argc, _TCHAR* argv[])
@@ -110,10 +148,10 @@ int _tmain(int argc, _TCHAR* argv[])
     setlocale(LC_ALL, "chs");
     list_files(files_arr, _T("E:\\–Ë«Û"));
     for (unsigned int i = 0; i < files_arr.size(); i++) {
-        wchar_t* file_path = (LPWSTR)(LPCTSTR)files_arr.at(i);
+        LPWSTR file_path = (LPWSTR)(LPCTSTR)files_arr.at(i);
         read_file(file_path);
     }
-
+    
 	return 0;
 }
 
