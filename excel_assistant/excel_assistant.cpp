6b082e1d26bfb3e5cb08226ddabafbba8f8f164f
victor <lncwwn@gmail.com>
2015-01-23 22:33:16 +0800

    finished version 1.0.0

--- excel_assistant/excel_assistant.cpp
+++ excel_assistant/excel_assistant.cpp
@@ -7,12 +7,15 @@
 #include "afx.h"
 #include "locale.h"
 #include "vector"
+#include "direct.h"
 #include "target.h"
+#define MAX_CSV_LINE 1000
+#define SPECIFIED_POSITION 14
 
 using namespace std;
-
 // read file
-void read_file(LPCWSTR file);
+void read_file(vector<CStringW> & pp_res, vector<CStringW> & lt_res,
+    vector<CStringW> & mt_res, vector<CStringW> & ht_res, LPCWSTR file);
 
 // list files in specified directory
 // and put each files into the passed vector container
@@ -23,12 +26,16 @@ vector<CStringW> split(CStringW str, char symbol);
 
 BOOL belongs_to(CStringW str, vector<CStringW> target_array);
 
-void _parse_line(CStringW line, short index);
+void _parse_line(vector<CStringW> & pp_res, vector<CStringW> & lt_res,
+    vector<CStringW> & mt_res, vector<CStringW> & ht_res, CStringW line, short index);
 
 // create a new file and write into the content
-void write_file();
+void write_folder(CStringW path);
 
-void read_file(LPCWSTR file) {
+void write_file(CStringW path, vector<CStringW> lines);
+
+void read_file(vector<CStringW> & pp_res, vector<CStringW> & lt_res,
+    vector<CStringW> & mt_res, vector<CStringW> & ht_res, LPCWSTR file) {
     FILE* fp;
     errno_t err_no;
     err_no = _wfopen_s(&fp, file, _T("r, ccs=UNICODE"));
@@ -36,7 +43,7 @@ void read_file(LPCWSTR file) {
         //wchar_t c;
         wchar_t line[1000];
         while ((fgetws(line, 1000, fp)) != NULL) {
-            _parse_line(line, 14);
+            _parse_line(pp_res, lt_res, mt_res, ht_res, line, SPECIFIED_POSITION);
         }
         //c = fgetwc(fp);
         /*while (c != WEOF) {
@@ -73,7 +80,8 @@ void list_files(vector<CStringW> & files_arr, LPCTSTR pstr) {
     finder.Close();
 }
 
-void _parse_line(CStringW line, short index) {
+void _parse_line(vector<CStringW> & pp_res, vector<CStringW> & lt_res,
+    vector<CStringW> & mt_res, vector<CStringW> & ht_res, CStringW line, short index) {
     CStringW copy_line = line;
     int fquote_index = line.Find('\"');
     if (fquote_index != -1) {
@@ -87,19 +95,35 @@ void _parse_line(CStringW line, short index) {
         line = CStringW(copy_line.Right(copy_line.GetLength() - fquote_index));
         CStringW right = CStringW(line.Right(line.GetLength() - len));
         line = CStringW(line.Left(len));
-        line.Replace('\,', '\t');
+        line.Replace(',', '\t');
         line = left + line + right;
     }
     
     vector<CStringW> arr = split(line, ',');
-    for (int i = 0; i < arr.size(); i++) {
+    for (unsigned int i = 0; i < arr.size(); i++) {
         if (arr.at(i).Find('\t') != -1) {
             arr.at(i).Replace('\t', ',');
         }
     }
     CStringW specifed_str = arr.at(index);
-    printf("%S\n", specifed_str);
+    vector<CStringW> _pp = split(PP, ',');
+    vector<CStringW> _lt = split(LT, ',');
+    vector<CStringW> _mt = split(MT, ',');
+    vector<CStringW> _ht = split(HT, ',');
 
+    if (belongs_to(specifed_str, _pp)) {
+        pp_res.push_back(line);
+        return;
+    } else if (belongs_to(specifed_str, _lt)) {
+        lt_res.push_back(line);
+        return;
+    } else if (belongs_to(specifed_str, _mt)) {
+        mt_res.push_back(line);
+        return;
+    } else if (belongs_to(specifed_str, _ht)) {
+        ht_res.push_back(line);
+        return;
+    }
 }
 
 BOOL belongs_to(CStringW str, vector<CStringW> target_array) {
@@ -120,38 +144,78 @@ vector<CStringW> split(CStringW str, char symbol) {
         len = str.GetLength();
         str = CStringW(str.Right(len - index - 1));
     }
-    result.push_back(str);
+    result.push_back(str.Trim());
     return result;
 }
 
-void write_file(CStringW path, CStringW line) {
+void write_folder(CStringW path) {
     FILE* fp;
-    errno_t err_no = _wfopen_s(&fp, path, _T("r"));
-    // no such file or directory
+    // for both reading and writing
+    errno_t err_no = _wfopen_s(&fp, path, _T("r+"));
+    // no such file or directory, then create new one
     if (err_no == 2) {
         if (!CreateDirectoryW(path, NULL)) {
             printf("create directory filed (%d)\n", GetLastError());
             return;
         }
     }
+}
 
-    //fwrite(&fp, sizeof(line), );
+void write_file(CStringW path, vector<CStringW> lines) {
+    printf("咱呆呆读完了, 现在正在写文件, 某胖等着啊...\n");
+    int i = 0, len = lines.size();
+    /*if (len < MAX_CSV_LINE) {
+        num = 1;
+    } else {
+        if (len % MAX_CSV_LINE) {
+            num = len / MAX_CSV_LINE + 1;
+        } else {
+            num = len / MAX_CSV_LINE;
+        }
+    }
+    CStringW temp;
+    for (int i = 0; i < num; i++) {
+        temp.Format(_T("%d\.csv"), i);
+        CStringW file_path = i + path;
+    }*/
+    FILE* fp;
+    if (_wfopen_s(&fp, (LPWSTR)(LPCTSTR)path, _T("w")) == 0) {
+        for (; i < len; i++) {
+            fprintf_s(fp, "%S", lines.at(i));
+            //fprintf_s(fp, "%c", '\n');
+        }
+        fclose(fp);
+    }
 }
 
 void init_env() {
-
+    
 }
 
 int _tmain(int argc, _TCHAR* argv[])
 {
     vector<CStringW> files_arr;
+    vector<CStringW> pp_res;
+    vector<CStringW> lt_res;
+    vector<CStringW> mt_res;
+    vector<CStringW> ht_res;
+
     setlocale(LC_ALL, "chs");
-    list_files(files_arr, _T("E:\\需求"));
+    list_files(files_arr, _T("E:\\files"));
+    printf("咱呆呆正在读文件, 等着啊某胖...\n");
     for (unsigned int i = 0; i < files_arr.size(); i++) {
         LPWSTR file_path = (LPWSTR)(LPCTSTR)files_arr.at(i);
-        read_file(file_path);
+        read_file(pp_res, lt_res, mt_res, ht_res, file_path);
     }
-    
+    write_folder(_T("E:\\files\\dist"));
+    if (pp_res.size() > 0)
+        write_file(_T("E:\\files\\dist\\gen_pp.csv"), pp_res);
+    if (lt_res.size() > 0)
+        write_file(_T("E:\\files\\dist\\gen_lt.csv"), lt_res);
+    if (mt_res.size() > 0)
+        write_file(_T("E:\\files\\dist\\gen_mt.csv"), mt_res);
+    if (ht_res.size() > 0)
+        write_file(_T("E:\\files\\dist\\gen_ht.csv"), ht_res);
 	return 0;
 }
 
